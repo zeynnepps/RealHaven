341b12efba02db0648d005ebf16b1536419f22d7
Akhil kumar <akhilkumar@Akhils-MacBook-Pro.local>
2025-03-10 15:57:37 -0700

    Added search API for properties with filtering by address, ZIP code, and property type

--- realheaven_backend/listings/urls.py
+++ realheaven_backend/listings/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PropertyListView, PropertyDetailView
+from .views import PropertyListView, PropertyDetailView,PropertySearchView
 
 urlpatterns = [
     path("properties/", PropertyListView.as_view(), name="property-list"),
     path("properties/<int:id>/", PropertyDetailView.as_view(), name="property-detail"),
+    path('properties/search/', PropertySearchView.as_view(), name='property-search'),
 ]
