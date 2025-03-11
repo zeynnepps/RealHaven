5e52f1f11da7e4e9c38c4bb0b954fd79c04c21a0
Akhil kumar <akhilkumar@Akhils-MacBook-Pro.local>
2025-03-11 11:27:42 -0700

    added image url instead of path

--- realheaven_backend/listings/serializers.py
+++ realheaven_backend/listings/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from .models import Property
+from django.conf import settings
 
 class PropertySerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Property
-        fields = "__all__"
+        fields = [
+            'id', 'street_address', 'city', 'state', 'zip_code', 
+            'price', 'bedrooms', 'bathrooms', 'square_footage', 
+            'property_type', 'image_url'
+        ]
+
+    def get_image_url(self, obj):
+        request = self.context.get('request')
+        if obj.image_path:
+            return request.build_absolute_uri(obj.image_path.url) if request else f"{settings.MEDIA_URL}{obj.image_path}"
+        return None
